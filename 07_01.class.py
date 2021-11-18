2f91bb027151cb6525941e0d72cea63e553f3b4f
Andrei Pervyi <taxidevelop@yandex.ru>
2021-11-18 20:46:03 +0400

    Lesson 7, classes and objects

---
+++ 07_01.class.py
@@ -0,0 +1,17 @@
+class Student:
+    """Create class Student
+    """
+
+    def __init__(self, name: str, surname: str):
+        self.name = name
+        self.surname = surname
+
+    def show(self):
+        print(f'Name: {self.name}, surname: {self.surname}!')
+
+
+Andrei = Student('Andrei', 'Pervyi')
+
+Andrei.show()
+
+print(dir(Student))
