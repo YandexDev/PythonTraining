a2038b895e852aa3827e0885a2c095bba17db6bd
Andrei Pervyi <taxidevelop@yandex.ru>
2021-12-26 00:27:06 +0400

    Lesson: Testing with doctest

--- fib.py
+++ fib.py
@@ -1,5 +1,21 @@
 def fib(n):
+    """Функция вычисляет числа Фибоначчи
+    >>> fib(1)
+    1
+    >>> fib(2)
+    1
+    >>> fib(3)
+    2
+    >>> fib(5)
+    10
+    """
     if n < 2:
         return n
     else:
         return fib(n - 1) + fib(n - 2)
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
