50c716118a72a6ffaca90ef0ab6ab278fa36aaf2
Andrei Pervyi <taxidevelop@yandex.ru>
2021-12-26 17:37:28 +0400

    lesson inittest

--- fib.py
+++ fib.py
@@ -1,5 +1,6 @@
 def fib(n):
     """Функция вычисляет числа Фибоначчи
+    n: целое число до 29
     >>> fib(1)
     1
     >>> fib(2)
@@ -9,10 +10,19 @@ def fib(n):
     >>> fib(5)
     10
     """
-    if n < 2:
-        return n
-    else:
-        return fib(n - 1) + fib(n - 2)
+    if not isinstance(n, int):
+        raise TypeError('Должно быть целое число')
+
+    if n < 0:
+        raise ValueError('Должно быть положительное число')
+
+    if n >= 30:
+        raise ValueError('Должно быть меньше и не равно 30')
+
+    F = [0, 1] + [0] * (n - 1)
+    for i in range(2, n + 1):
+        F[i] = F[i - 1] + F[i - 2]
+    return F[n]
 
 
 if __name__ == "__main__":
---
+++ unittest_2.py
@@ -0,0 +1,30 @@
+from unittest import TestCase, main
+
+from fib import fib
+
+
+class TestFibonacci(TestCase):
+    def test_simple(self):
+        for param, result in [(0, 0), (1, 1), (3, 2), (10, 55)]:
+            self.assertEqual(fib(param), result)
+
+    def test_stress(self):
+        self.assertEqual(fib(29), (fib(27) + fib(28)))
+        with self.assertRaises(ValueError):
+            fib(30)
+
+    def test_negative(self):
+        with self.assertRaises(ValueError):
+            fib(-1)
+        with self.assertRaises(ValueError):
+            fib(-100)
+
+    def test_wrong_param_type(self):
+        with self.assertRaises(TypeError):
+            fib('Hello')
+        with self.assertRaises(TypeError):
+            fib(2.5)
+
+
+if __name__ == "__main__":
+    main()
