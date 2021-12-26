ba20a5c64690422443e11ca13c9879d6682fe8f3
Andrei Pervyi <taxidevelop@yandex.ru>
2021-12-26 22:44:59 +0400

    Add new files with OOP folder

---
+++ extended_python/OOP_attributes.py
@@ -0,0 +1,31 @@
+class Point:
+    """Класс представления точек на плоскости"""
+
+    color = 'Red'
+    circle = 2
+
+
+print(Point.color)
+print(Point.circle)
+print(Point.__dict__)  # Выводит все свойства (атрибуты) класса
+
+a = Point()
+b = Point()
+
+a.color = 'Black'  # Переопределил свойство у объекта а класса Point
+print(a.color)
+print(b.color)
+print(a.__dict__)  # Появилось свойство у объекта а
+print(b.__dict__)
+
+Point.type_pt = 'Name'  # Добавили новое свойста классу
+print(Point.type_pt)
+setattr(Point, 'name', 'Andrei')  # Добавили новое свойста классу
+print(Point.name)
+
+# Вернет False, если нет такого атрибута
+print(getattr(Point, 'lenght', False))
+# Показать наличие атрибута
+print(hasattr(Point, 'type_pt'))
+# del Point.name Удаление атрибута класса
+# delattr(Point, 'name') Удаление атрибута через функцию
