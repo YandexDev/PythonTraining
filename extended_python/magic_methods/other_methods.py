15b79d2b553506e118ad5cd404d72b4b90a49cae
Andrei Pervyi <taxidevelop@yandex.ru>
2021-12-29 02:32:34 +0400

    Other methods

---
+++ extended_python/magic_methods/other_methods.py
@@ -0,0 +1,16 @@
+"""
+https://www.youtube.com/watch?v=a2L5vyCUvzo&list=PLA0M1Bcd0w8zPwP7t-FgwONhZOHt9rz9E&index=18&ab_channel=selfedu
+Функция bool() и магические методы
+- __len__() - вызывается функцией bool(), если не определен магический
+метод __bool__()
+- __bool__() - вызывается в приоритетном порядке функцией bool()
+
+https://www.youtube.com/watch?v=EAoiOwYQSuY&list=PLA0M1Bcd0w8zPwP7t-FgwONhZOHt9rz9E&index=19&ab_channel=selfedu
+__getitem__(self, item) - получение значения по ключу item
+__setitem__(self, key, value) - запись значения value по ключу key
+__delitem__(self, key) - удаление элемента по ключу key
+
+https://www.youtube.com/watch?v=SDJ-Vmf_pxk&list=PLA0M1Bcd0w8zPwP7t-FgwONhZOHt9rz9E&index=20&ab_channel=selfedu
+__iter__(self) - получение итератора для перебора объекта
+__next__(self) - переход к следующему значению и его считывание
+"""
